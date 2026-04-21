e2320f742bec058593abeb28f0865b1ee3970f35
Sohag <ahadulsohag@gmail.com>
2026-04-21 22:49:59 +0600

    button added

---
+++ lib/features/auth/presentation/widgets/social_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import '../../../../core/constants/app_colors.dart';
+import '../../../../core/constants/app_dimensions.dart';
+
+class SocialButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onPressed;
+
+  const SocialButton({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      borderRadius: BorderRadius.circular(AppDimensions.radiusM),
+      child: Container(
+        width: AppDimensions.socialButtonWidth,
+        height: AppDimensions.socialButtonHeight,
+        decoration: BoxDecoration(
+          border: Border.all(color: AppColors.border),
+          borderRadius: BorderRadius.circular(AppDimensions.radiusM),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: AppDimensions.iconM,
+              color: AppColors.textSecondary,
+            ),
+            const SizedBox(width: AppDimensions.paddingS),
+            Text(
+              label,
+              style: const TextStyle(
+                color: AppColors.textSecondary,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
